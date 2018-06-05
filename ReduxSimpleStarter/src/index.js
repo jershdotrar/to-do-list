404498b93dad7ceca118328cc94e0b2ecdebf13c
jersh <zombine.rar@gmail.com>
2018-06-05 17:42:45 -0500

    Updated simplestarter

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,27 +1,45 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAG5RTwrZTOLn34n4yWusLIOYRZ2n6t1YI';
 
 
 class App extends Component{
   constructor( props ) {
     super( props );
-    this.state = { videos: [] };
-    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => this.setState({ videos }) );
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+    
+    this.videoSearch('reposted in the wrong neighborhood');
+  }
+
+  videoSearch( term ) {
+    YTSearch({ key: API_KEY, term: term }, videos => this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+   }) );
   }
 
   render( ) {
+const videoSearch = _.debounce(( term ) => { this.videoSearch( term ) }, 500);
+
     return (
       <div>
-        <SearchBar />
-        <VideoList videos = { this.state.videos } />
+        <SearchBar onSearchTermChange = { videoSearch } />
+        <VideoDetail video = { this.state.selectedVideo } />
+        <VideoList
+            onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
+            videos = { this.state.videos } />
       </div>
     );
   }
 }
 
-ReactDOM.render( <App />, document.querySelector( '.container' ));
+ReactDOM.render( <App />, document.querySelector( '.container' ) );
 
